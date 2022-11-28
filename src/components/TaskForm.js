c57c53e018ccc374ca79a791640043c6c65952ea
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-11-28 13:22:26 -0400

    feat:setting basic for form

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const TaskForm = () => {
-  return (
-    <div>TaskForm</div>
-  )
-}
+  const [task, setTasks] = useState({
+    title: "",
+    description: "",
+  });
+  const handleChange = (e) => {
+    setTasks({
+      ...task,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-export default TaskForm
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(task);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="title"
+        type="text"
+        placeholder="Titulo"
+        onChange={handleChange}
+      />
+      <textarea
+        name="description"
+        placeholder="descripcion"
+        onChange={handleChange}
+      />
+      <button>Guardar</button>
+    </form>
+  );
+};
+
+export default TaskForm;
